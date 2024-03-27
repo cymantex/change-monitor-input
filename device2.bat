7c31036415359cdc322a3dbb83117bafb1b68cee
Simon Eriksson <simon.eriksson@entiros.se>
2024-03-27 20:26:50 +0100

    SetValue over SetValueIfNeeded

--- device2.bat
+++ device2.bat
@@ -1,2 +1,2 @@
-ControlMyMonitor.exe /SetValueIfNeeded "\\.\DISPLAY2\Monitor0" 60 17
-ControlMyMonitor.exe /SetValueIfNeeded "\\.\DISPLAY1\Monitor0" 60 15
+ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 17
+ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 15
