8b81b0616a14a5544ccd170eb0ef36ba10421d3d
Simon Eriksson <simon.eriksson@entiros.se>
2024-03-27 18:42:41 +0100

    Add laptop device input switch scripts

---
+++ laptop-device1.bat
@@ -0,0 +1,2 @@
+ControlMyMonitor.exe /SetValue "\\.\DISPLAY5\Monitor0" 60 17
+ControlMyMonitor.exe /SetValue "\\.\DISPLAY6\Monitor0" 60 15
---
+++ laptop-device2.bat
@@ -0,0 +1,2 @@
+ControlMyMonitor.exe /SetValue "\\.\DISPLAY5\Monitor0" 60 15
+ControlMyMonitor.exe /SetValue "\\.\DISPLAY6\Monitor0" 60 17
