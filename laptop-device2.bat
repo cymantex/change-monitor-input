aad17c6803f89c1a5b411d556d9e860fbafe6e56
Simon Eriksson <sim.eriksson@outlook.com>
2024-03-27 20:20:12 +0100

    Remove .exe files as Logi Options actually supports .bat script although it doesnt seem like it at first

--- laptop-device2.bat
+++ laptop-device2.bat
@@ -1,2 +1,2 @@
-ControlMyMonitor.exe /SetValue "\\.\DISPLAY5\Monitor0" 60 15
-ControlMyMonitor.exe /SetValue "\\.\DISPLAY6\Monitor0" 60 17
+ControlMyMonitor.exe /SetValueIfNeeded "\\.\DISPLAY5\Monitor0" 60 15
+ControlMyMonitor.exe /SetValueIfNeeded "\\.\DISPLAY6\Monitor0" 60 17
